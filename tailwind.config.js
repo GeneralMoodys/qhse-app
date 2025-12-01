1bca97dce00a98ead17e18930a53487f73fc9d74
syamban14 <syamsulikj.48@gmail.com>
2025-12-01 09:26:29 +0700

    memperbarui tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ import forms from '@tailwindcss/forms';
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/livewire/livewire/src/Features/SupportPagination/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
